46cddde4db4353df4661b27cbf77f78f55a30b25
Jacob Delott <jldelott@gmail.com>
2023-09-19 20:54:27 -0700

    made corrections to timer

--- src/Timer.js
+++ src/Timer.js
@@ -63,7 +63,8 @@ function Timer() {
       </div>
       <div className="mt-4">
         <label className="timer-label">
-        <h2 className="timer-heading font-bold set-timer-duration-text">Set Timer Duration</h2>
+     
+        <h2 className="timer-heading font-bold set-timer-duration-text">Set Timer in Minutes:</h2>
         <input
             type="number"
             value={totalSeconds / 60}
@@ -71,6 +72,7 @@ function Timer() {
             className="mt-2 border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring focus:border-blue-500 w-20 input"
           />
         </label>
+        {/* <div><span className="text-gray-500 ml-1 minutes-indicator">Minutes</span> </div> */}
       </div>
     </div>
   );
